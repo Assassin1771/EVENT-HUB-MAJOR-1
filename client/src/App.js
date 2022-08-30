112c4a544e0f68ca1ccddbe4075f421da0b0b9e1
Tushar Garg <tshrgarg2010@gmail.com>
2022-08-30 23:34:44 +0530

    Basic Mern Setup

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ const App = () => {
       </video>
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          MAJOR PROJECT
+          EVENTIFY
         </Typography>
         {/* <img className={classes.image} src={memories} alt="icon" height="60" /> */}
       </AppBar>
---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("server start");
+});
+
+// const CONNECTION_URL =
+//   "mongodb+srv://admin:admin@cluster0.kwwcd.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.kwwcd.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
